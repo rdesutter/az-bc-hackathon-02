638d578a75c910e6ec9e48e61cb7d5089842766e
Robbie De Sutter <rdesutter@microsoft.com>
2024-04-22 10:26:14 +0200

    speechConfig

--- src/js/main.js
+++ src/js/main.js
@@ -273,7 +273,7 @@ window.stopSession = () => {
 }
 
 window.startRecording = () => {
-  const speechConfig = SpeechSDK.SpeechConfig.fromAuthorizationToken(token, 'westeurope');
+  const speechConfig = SpeechSDK.SpeechConfig.fromAuthorizationToken(token, 'swedencentral');
   speechConfig.authorizationToken = token;
   speechConfig.SpeechServiceConnection_LanguageIdMode = "Continuous";
   var autoDetectSourceLanguageConfig = SpeechSDK.AutoDetectSourceLanguageConfig.fromLanguages(supported_languages);
